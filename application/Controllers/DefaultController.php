fc088db446b769439cbb6124f2a7a60c4bd4452b
Luis Espino <luis@espino.info>
2019-07-07 19:44:58 -0500

    First commit

---
+++ application/Controllers/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Controllers;
+
+use RSPhp\Framework\Controller;
+use RSPhp\Framework\View;
+use RSPhp\Framework\Input;
+use RSPhp\Framework\Uri;
+use Exception;
+
+/**
+ * Default controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Creates a new instance of DefaultController
+     */
+    function __construct()
+    {
+    } // end function constructs
+
+    /**
+     * The home %baseUrl/Default/
+     */
+    function index()
+    {
+    } // end function index
+
+} // end class DefaultController;
